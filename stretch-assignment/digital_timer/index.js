8a686a579ed95c63706c44557770edfa7dc42977
Pedro Prieto <pedro.prieto003@gmail.com>
2019-09-30 16:40:19 -0400

    Initial for stretch

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,12 @@
+let wholeDigits = document.querySelector(".digits")
+
+let secondTens = document.querySelector("#secondTens");
+let secondOnes = document.querySelector("#secondOnes");
+let colon = document.querySelector("#colon");
+let msHundreds = document.querySelector("#msHundreds");
+let msTens = document.querySelector("#msTens");
+
+secondTens.textContent = 0;
+secondOnes.textContent = 0;
+msHundreds.textContent = 0;
+msTens.textContent = 0;
