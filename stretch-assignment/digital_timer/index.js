1ce7c24c4cab6a4db746cb2b0677bd2df2cf5a3c
Pedro Prieto <pedro.prieto003@gmail.com>
2019-09-30 17:47:36 -0400

    Got counter working, need to fix reset btn

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -6,7 +6,50 @@ let colon = document.querySelector("#colon");
 let msHundreds = document.querySelector("#msHundreds");
 let msTens = document.querySelector("#msTens");
 
-secondTens.textContent = 0;
-secondOnes.textContent = 0;
-msHundreds.textContent = 0;
-msTens.textContent = 0;
+let start = document.querySelector(".start");
+let reset = document.querySelector(".reset")
+
+
+// secondTens.textContent = 0;
+// secondOnes.textContent = 0;
+// msHundreds.textContent = 0;
+// msTens.textContent = 0;
+
+// let countUp = function(time) {
+//     time.textContent = Number(time.textContent) + 1;
+//     return time;
+// }
+
+function startOnClick(){
+    secondTens.textContent = 0;
+    secondOnes.textContent = 0;
+    msHundreds.textContent = 0;
+    msTens.textContent = 0;
+    let counter = setInterval(function(){
+        msTens.textContent = Number(msTens.textContent) + 1;
+        if(msTens.textContent == 10){
+            msTens.textContent = Number(msTens.textContent) - 10;
+            msHundreds.textContent = Number(msHundreds.textContent) + 1;
+            if(msHundreds.textContent == 10){
+                msHundreds.textContent = Number(msHundreds.textContent) - 10;
+                secondOnes.textContent = Number(secondOnes.textContent) + 1;
+            }
+            if(secondOnes.textContent == 10){
+                secondOnes.textContent = Number(secondOnes.textContent) - 10;
+                secondTens.textContent = Number(secondTens.textContent) + 1;
+                clearInterval(counter);
+                wholeDigits.classList.add("redDigit");
+            }
+        };
+},10);
+    return counter;
+}
+
+
+start.addEventListener("click", startOnClick);
+reset.addEventListener("click", function(e){
+    secondTens.textContent = 0;
+    secondOnes.textContent = 0;
+    msHundreds.textContent = 0;
+    msTens.textContent = 0;
+})
