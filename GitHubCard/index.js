96b43067a6f2411238aa5a17bc84ebf63fb4a696
Pedro Prieto <pedro.prieto003@gmail.com>
2019-10-03 15:53:52 -0400

    MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,11 +2,7 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios.get("https://api.github.com/users/prietop97")
-  .then(response =>{
-    let data = response.data;
-    console.log(data);
-}) 
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -28,7 +24,7 @@ axios.get("https://api.github.com/users/prietop97")
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -49,7 +45,7 @@ const followersArray = [];
 </div>
 
 */
-function CreateACard(obj){
+let CreateACard = (obj)=> {
   /// CREATING ELEMENTS
   let card = document.createElement("div");
   let img = document.createElement("img");
@@ -63,6 +59,24 @@ function CreateACard(obj){
   let following = document.createElement("p");
   let bio = document.createElement("p");
 
+  //// ADDING CLASSES
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  name.classList.add("name");
+  username.classList.add("username");
+
+  //// GIVING CONTEXT
+  img.src = obj.avatar_url;
+  name.textContent = obj.name;
+  username.textContent = obj.login;
+  location.textContent = `Location: ${obj.location}`;
+  profile.textContent = "Profile: ";
+  profileLink.href = obj.html_url;
+  profileLink.textContent = obj.html_url;
+  followers.textContent = `Followers: ${obj.followers}`;
+  following.textContent = `Following: ${obj.following}`;
+  bio.textContent = obj.bio;
+
   /// APPENDING CHILDS
   card.appendChild(img);
   card.appendChild(cardInfo);
@@ -75,23 +89,30 @@ function CreateACard(obj){
   cardInfo.appendChild(bio);
   profile.appendChild(profileLink);
 
-  //// ADDING CLASSES
-  card.classList.add("card");
-  cardInfo.classList.add("card-info");
-  name.classList.add("name");
-  username.classList.add("username");
+  return card;
+};
 
-  //// GIVING CONTEXT
+let cardContainer = document.querySelector(".cards")
+// axios.get('https://api.github.com/users/prietop97')
+//   .then(response => {
+//     console.log(response.data)
+//     cardContainer.appendChild(CreateACard(response.data))
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
 
-  
-
-
-
-
-
-}
-
-
+  const followersArray = ['prietop97','tetondan','dustinmyers','justsml','luishrd','bigknell'];
+  followersArray.forEach(value=>{
+    axios.get(`https://api.github.com/users/${value}`)
+  .then(response => {
+    console.log(response.data)
+    cardContainer.appendChild(CreateACard(response.data))
+  })
+  .catch(err => {
+    console.log(err)
+  })
+  })
 
 /* List of LS Instructors Github username's: 
   tetondan
