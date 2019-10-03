d61994a664aef0a45ae9ca924a912875c9538110
Pedro Prieto <pedro.prieto003@gmail.com>
2019-10-03 14:56:06 -0400

    eh

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,11 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios.get("https://api.github.com/users/prietop97")
+  .then(response =>{
+    let data = response.data;
+    console.log(data);
+}) 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,6 +49,49 @@ const followersArray = [];
 </div>
 
 */
+function CreateACard(obj){
+  /// CREATING ELEMENTS
+  let card = document.createElement("div");
+  let img = document.createElement("img");
+  let cardInfo = document.createElement("div");
+  let name = document.createElement("h3");
+  let username = document.createElement("p");
+  let location = document.createElement("p");
+  let profile = document.createElement("p");
+  let profileLink = document.createElement('a');
+  let followers = document.createElement("p");
+  let following = document.createElement("p");
+  let bio = document.createElement("p");
+
+  /// APPENDING CHILDS
+  card.appendChild(img);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  profile.appendChild(profileLink);
+
+  //// ADDING CLASSES
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  name.classList.add("name");
+  username.classList.add("username");
+
+  //// GIVING CONTEXT
+
+  
+
+
+
+
+
+}
+
+
 
 /* List of LS Instructors Github username's: 
   tetondan
