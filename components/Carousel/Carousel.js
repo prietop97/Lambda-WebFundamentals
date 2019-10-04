1ac77cdc579002d2c230e466c72acea932c72e23
Pedro Prieto <pedro.prieto003@gmail.com>
2019-10-04 12:26:15 -0400

    carousel Component

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,41 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+
+function CreateACarousel(){
+  let carousel = document.createElement("div");
+  let leftBtn = document.createElement("div");
+  let img1 = document.createElement("img");
+  let img2 = document.createElement('img');
+  let img3 = document.createElement("img");
+  let img4 = document.createElement("img");
+  let rightbtn = document.createElement("div");
+
+  img1.src = "./assets/carousel/mountains.jpeg";
+  img2.src = "./assets/carousel/computer.jpeg";
+  img3.src = "./assets/carousel/trees.jpeg";
+  img4.src = "./assets/carousel/turntable.jpeg";
+
+  carousel.appendChild(leftBtn);
+  carousel.appendChild(img1);
+  carousel.appendChild(img2);
+  carousel.appendChild(img3);
+  carousel.appendChild(img4);
+  carousel.appendChild(rightbtn);
+
+
+  carousel.classList.add("carousel");
+  leftBtn.classList.add("left-button");
+  rightbtn.classList.add("right-button");
+
+
+  leftBtn.innerHTML = "<i class=\"fas fa-chevron-left\"></i>";
+  rightbtn.innerHTML = "<i class=\"fas fa-chevron-right\"></i>";
+
+
+  return carousel;
+
+}
+
+document.querySelector(".carousel-container").appendChild(CreateACarousel());
