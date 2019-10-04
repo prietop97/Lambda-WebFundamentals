b8a2e68f94412433463b23c7f547c7e5f4fcc17e
Pedro Prieto <pedro.prieto003@gmail.com>
2019-10-04 13:43:43 -0400

    Commented Out Carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -19,40 +19,80 @@
 */
 
 
-function CreateACarousel(){
-  let carousel = document.createElement("div");
-  let leftBtn = document.createElement("div");
-  let img1 = document.createElement("img");
-  let img2 = document.createElement('img');
-  let img3 = document.createElement("img");
-  let img4 = document.createElement("img");
-  let rightbtn = document.createElement("div");
-
-  img1.src = "./assets/carousel/mountains.jpeg";
-  img2.src = "./assets/carousel/computer.jpeg";
-  img3.src = "./assets/carousel/trees.jpeg";
-  img4.src = "./assets/carousel/turntable.jpeg";
-
-  carousel.appendChild(leftBtn);
-  carousel.appendChild(img1);
-  carousel.appendChild(img2);
-  carousel.appendChild(img3);
-  carousel.appendChild(img4);
-  carousel.appendChild(rightbtn);
+// function CreateACarousel(){
+//   let carousel = document.createElement("div");
+//   let leftBtn = document.createElement("div");
+//   let img1 = document.createElement("img");
+//   let img2 = document.createElement('img');
+//   let img3 = document.createElement("img");
+//   let img4 = document.createElement("img");
+//   let rightbtn = document.createElement("div");
 
 
-  carousel.classList.add("carousel");
-  leftBtn.classList.add("left-button");
-  rightbtn.classList.add("right-button");
-  img1.classList.toggle("toggle");
+//   carousel.appendChild(leftBtn);
+//   carousel.appendChild(img1);
+//   carousel.appendChild(img2);
+//   carousel.appendChild(img3);
+//   carousel.appendChild(img4);
+//   carousel.appendChild(rightbtn);
+
+//   img1.src = "./assets/carousel/mountains.jpeg";
+//   img2.src = "./assets/carousel/computer.jpeg";
+//   img3.src = "./assets/carousel/trees.jpeg";
+//   img4.src = "./assets/carousel/turntable.jpeg";
+
+//   carousel.classList.add("carousel");
+//   leftBtn.classList.add("left-button");
+//   rightbtn.classList.add("right-button");
+
+//   leftBtn.innerHTML = "<i class=\"fas fa-chevron-left\"></i>";
+//   rightbtn.innerHTML = "<i class=\"fas fa-chevron-right\"></i>";
+
+//   img1.classList.toggle("toggle");
+//   let imgArr = [img1,img2,img3,img4];
+
+//   rightbtn.addEventListener("click", ()=>{
+//     if(current === imgArr.length-1){
+//       current = 0;
+//     }
+//     else{
+//        current = current + 1;
+//    }
+//     function next(){
+//       // if(current > imgArr.length){
+//       //   current = 0;
+//       // }else{
+//       //   current = current + 1;
+//       // }
+//       imgArr[current].classList.toggle("toggle");
+//       imgArr[current-1].classList.toggle("toggle")
+//     }
+//     console.log(current)
+//     return next();
+//   })
+//   // leftBtn.addEventListener("click", ()=>{
+//   //   // if(current === imgArr[0]){
+//   //   //   current = imgArr[imgArr.length-1];
+//   //   // }
+//   //   function previous(){
+//   //     if(current === imgArr[0]){
+//   //       current = imgArr[imgArr.length - 1];
+//   //     }else{
+//   //       current = current - 1;
+//   //     }
+//   //     imgArr[current].classList.toggle("toggle");
+//   //     imgArr[current-1].classList.toggle("toggle")
+//   //   }
+//   //   console.log(current)
+//   //   return previous();
+//   // })
+//   let current = 0
+    
 
 
-  leftBtn.innerHTML = "<i class=\"fas fa-chevron-left\"></i>";
-  rightbtn.innerHTML = "<i class=\"fas fa-chevron-right\"></i>";
 
+//   return carousel;
 
-  return carousel;
+// }
 
-}
-
-document.querySelector(".carousel-container").appendChild(CreateACarousel());
+// document.querySelector(".carousel-container").appendChild(CreateACarousel());
